ec88364194c63225e9f68b165feecf7e523fbfc9
Gabriel <53486357+GabMoreiraSilva@users.noreply.github.com>
2023-02-13 14:20:05 -0300

    perf: incrimentando a tabela de log no usuário e filtrando a saida de todos os usuários

--- bd.js
+++ bd.js
@@ -4,7 +4,9 @@ require('dotenv').config()
 mongoose.connect(`mongodb+srv://gabmoreira:${process.env.SENHA}@freecodecamp-gmoreira.5mklk3n.mongodb.net/?retryWrites=true&w=majority`)
 
 const User = new mongoose.Schema({
-  username:  {type: String, required: true}
+  username:  {type: String, required: true},
+  count: {type: Number},
+  log: []
 },{ versionKey: false });
 
 const Exercise = new mongoose.Schema({
@@ -14,14 +16,7 @@ const Exercise = new mongoose.Schema({
   date: { type: Date, default: Date.now }
 },{ versionKey: false });
 
-const Log = new mongoose.Schema({
-  username: {type: User},
-  count: {type: Number},
-  log: [{type : Exercise}]
-},{ versionKey: false });
-
 const user = mongoose.model("user", User);
 const exercise = mongoose.model("exercise", Exercise);
-const log = mongoose.model("log", Log);
 
-module.exports = {user, exercise, log};
+module.exports = {user, exercise};
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 var bodyParser = require('body-parser')
-const {user, exercise, log} = require("./bd.js")
+const {user, exercise} = require("./bd.js")
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,17 +13,35 @@ app.get('/', (req, res) => {
 });
 
 // I know is kind messy, but it works fine and i try to use the 42 indentation, is the easiest way to see the indentation well
-app.route("/api/users").post((req, res) => 
-{
+app.route("/api/users").post((req, res) => {
   const new_user = new user(req.body);
   new_user.save((err, result) =>
+  {
+    if (err)
+      return res.status(204).send({error: err});
+    res.status(201).send({username: result.username, _id: result._id});
+  })
+}).get((req, res) => {
+  user.find().select("username user._id").exec((err, result) => 
+  {
+    if (err)
+      return res.status(204).send({error: err});
+    res.status(200).send(result)
+  })
+})
+
+app.route('/api/users/:_id/exercises').post((req, res) =>{
+  const new_exercise = new exercise(req.body)
+  if (new_exercise === '')
+    new_exercise = new Date().toISOString().substring(0, 10);
+  new_exercise.save((err, result) =>
   {
     if (err)
       return res.status(204).send({error: err});
     res.status(201).send(result);
   })
 }).get((req, res) => {
-  user.find((err, result) => 
+  exercise.findById((req.params.id),(err, result) => 
   {
     if (err)
       return res.status(204).send({error: err});
